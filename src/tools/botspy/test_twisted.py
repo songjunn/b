0bbce5172f83a8b1af177f0e80eaacd7a1ee9633
songjundev <songjun0810@163.com>
2016-02-23 00:42:09 +0800

    twisted

--- src/tools/botspy/test_twisted.py
+++ src/tools/botspy/test_twisted.py
@@ -1,20 +1,27 @@
 # coding: utf-8
+import logging
 import threading
-import message
+import protocolpack
 from twisted.internet import reactor, protocol
 
 class NetClient(protocol.Protocol):   
     def connectionMade(self):
     	print "connection made."
-        #self.transport.write("hello, world!")
+        self.sendData(9, "hello python")
     
     def dataReceived(self, data):
         "As soon as any data is received, write it back."
         print "Received:", data
-        self.transport.loseConnection()
+        # self.transport.loseConnection()
     
     def connectionLost(self, reason):
         print "connection lost:", reason
+
+    def sendData(self, type, data):
+        message = protocolpack.Protocol()
+        buffer = message.package(type, data)
+        self.transport.write(buffer)
+        logging.debug("Send message %d size %d: %s", type, len(buffer), data)
 
 class NetFactory(protocol.ClientFactory):
     protocol = NetClient
@@ -39,10 +46,21 @@ class Bots():
    		while (True):
    			count = 1
 
+def init_logging():
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    sh = logging.StreamHandler()
+    formatter = logging.Formatter(
+        '%(asctime)s %(module)s-L%(lineno)d-%(levelname)s: %(message)s')
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+    logging.info(
+        "Set logging level: %s", logging.getLevelName(logger.getEffectiveLevel()))
+
 # this connects the protocol to a server running on port
 def main():
     bots = Bots()
-    bots.startNetwork("192.168.6.47", 20900)
+    bots.startNetwork("221.228.207.92", 20900)
 
 # this only runs if the module was *not* imported
 if __name__ == '__main__':
